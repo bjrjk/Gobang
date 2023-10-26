224d20830d65fdc13bc407b201fc120fbb988e95
Jack Ren <bjrjk@qq.com>
2023-10-26 20:50:16 +0800

    Updated .gitignore & Added gridTest.cpp into compile.sh

--- compile.sh
+++ compile.sh
@@ -8,6 +8,10 @@ if [ 0"$CXX" = "0" ]; then
     export CXX=g++
 fi
 
+# Executables
 $CXX gobang.cpp -o gobang -Ofast --std=c++17
 $CC -c exec.c -o exec.o -O2
 $CXX exec.o main.cpp -o main -O2
+
+# Tests
+$CXX gridTest.cpp -o gridTest -g
