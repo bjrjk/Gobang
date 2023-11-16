8b93a607845631f73fb745b2633c90bbaa7d1f8e
Jack Ren <bjrjk@qq.com>
2023-11-16 22:28:14 +0800

    Some part of code for ForbiddenMoveJudger::isForbiddenMove()

--- forbiddenMove.cpp
+++ forbiddenMove.cpp
@@ -1,5 +1,37 @@
+#include <algorithm>
 #include "forbiddenMove.h"
 
+static bool AliveThreeJudger();
+static bool AliveFourJudger();
+static bool OneSideBlockingFourJudger();
+static bool LongContiguousJudger();
+
+static bool (*ForbiddenChessChainJudger[])() = {
+    nullptr,
+    AliveThreeJudger,
+    AliveFourJudger,
+    OneSideBlockingFourJudger,
+    LongContiguousJudger
+};
+
+ForbiddenChessChainType ForbiddenMoveJudger::isAnyForbiddenChessChain(SingleChessChainStatus & status) {
+
+}
+
 bool ForbiddenMoveJudger::isForbiddenMove(ChessPiece piece, int x, int y) {
-    
+    constexpr int ChessChainNumber = 4;
+    SingleChessChainStatus multipleDirectionChessChainStatus[ChessChainNumber] = {
+        SingleChessChainStatus(piece, ChessPosition(x, y), LINE),
+        SingleChessChainStatus(piece, ChessPosition(x, y), ROW),
+        SingleChessChainStatus(piece, ChessPosition(x, y), ULLRDiagonal),
+        SingleChessChainStatus(piece, ChessPosition(x, y), LLURDiagonal)
+    };
+    ForbiddenChessChainType forbiddenChessChainType4DifferentDirections[ChessChainNumber];
+    for (int i = 0; i < ChessChainNumber; i++)
+        forbiddenChessChainType4DifferentDirections[i] = isAnyForbiddenChessChain(multipleDirectionChessChainStatus[i]);
+    unsigned aliveThreeCount = std::count_if(forbiddenChessChainType4DifferentDirections,
+        forbiddenChessChainType4DifferentDirections + ChessChainNumber,
+        [](ForbiddenChessChainType type) { type == ForbiddenChessChainType::AliveThree;}
+    );
+
 }
--- forbiddenMove.h
+++ forbiddenMove.h
@@ -2,9 +2,18 @@
 #include "gobang.h"
 #include "grid.hpp"
 
+enum class ForbiddenChessChainType {
+    None, // 未形成禁手局势
+    AliveThree, // 活三：连三或跳三
+    AliveFour, // 活四：紧密相连的同色四子
+    OneSideBlockingFour, // 冲四：除活四外可以形成五子的情况
+    LongContiguous, // 长连禁手
+};
+
 class ForbiddenMoveJudger {
 private:
     ChessboardGrid & grid;
+    ForbiddenChessChainType isAnyForbiddenChessChain(SingleChessChainStatus &);
 public:
     ForbiddenMoveJudger(ChessboardGrid & grid): grid(grid) {}
     bool isForbiddenMove(ChessPiece, int, int);
